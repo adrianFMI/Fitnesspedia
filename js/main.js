52e2f967679ee61d12ab15924c91d747b6ab1d19
adrianFMI <47193253+adrianFMI@users.noreply.github.com>
2019-03-19 10:30:26 -0700

    creation of the project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict"
+$(document).ready(function () {
+    console.log("doc ready");
+    var calMen = 2500; //amount of calories a men should eat a day
+    var calWomen = 2000; //amount of calories a women should eat a day
+    var cal = 0.00012959782; //amount of kilo = to 1 calorie
+    var kg = 7716.18; //amount of calories = to 1 kg
+    var feet = 0.3038; //value of feets in meters
+    var meter = 3.28084; // value of meters in feets
+    var kilo = 2.20462; //value of kilos in pounds
+    var lbs = 0.453592; // value of punds in kilos
+    var feetInch = 12; // 1 feet = to 12 inches
+    var inch = 0.0833333; // value of 1 inch in feets
+});
