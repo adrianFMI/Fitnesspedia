d7dba13c594c731c6dc67bcc57229da5871c27f5
adrianFMI <47193253+adrianFMI@users.noreply.github.com>
2019-03-19 17:59:49 -0700

    New funtion for calculators

--- js/main.js
+++ js/main.js
@@ -11,8 +11,16 @@ $(document).ready(function () {
     var lbs = 0.453592; // value of punds in kilos
     var feetInch = 12; // 1 feet = to 12 inches
     var inch = 0.0833333; // value of 1 inch in feets
+    var foods = ["Apple(4oz)", "Banana(6oz.)", "Orange(4oz.)", "Cucumber(4oz)", "Carrots(1cup)", "Brocolli(1cup)", "Beef(2oz.)", "Chicken(2oz.)", "Pizza(slice)", "Beer(1can)", "Coke(1can)"];
+    var exercises = ["Walk", "Swimming", "Running", "Bicycling", "Football", "Basketball", "Soccer"];
+    for (var i = 0; i < foods.length; i++) {//fills the option for food dropmenu
+        $("#food").append(`<option>${foods[i]}</option>`);
+    }
+    for (var x = 0; x < exercises.length; x++) {//fill the options for exercise dropmenu
+        $("#exercise").append(`<option>${exercises[x]}</option>`);
+    }
 
-    $("#unit").change(function () {
+    $("#unit").change(function () {//Change the value of  metric units in thelabels for BMI
         var unit = $("#unit").val();
         if (unit === "Pound-Feet") {
             $("#peso").html("Pounds");
@@ -24,7 +32,7 @@ $(document).ready(function () {
             $("#altura2").html("Centimer(s)");
         }
     });
-    $("#bmi").click(function () {
+    $("#bmi").click(function () {//calculates your BMI depending the metric units you use
         var unit = $("#unit").val();
         var height = parseInt($("#height").val());
         var height2 = parseInt($("#height2").val());
@@ -33,15 +41,35 @@ $(document).ready(function () {
             var tInch = (height * 12) + height2;
             var bmi = 703 * (weight / (tInch * tInch));
             $("#result").html(bmi);
-        } else{
-            var tHeight=height+(height2/100);
+        } else {
+            var tHeight = height + (height2 / 100);
             console.log(height2);
-            var bmi = weight/(tHeight*tHeight);
+            var bmi = weight / (tHeight * tHeight);
             $("#result").html(bmi);
         }
     });
-    //men 10(weight kg)+6.25(height cm) - 5(edad) +5
+    //men 10(weight kg)+6.25(height cm) - 5(edad) +5  formulas to calculate right amount of calories for men and women
     //women 10(weight kg)+6.25(height cm) - 5(edad) -161
-    
-    
+    $("#metric").change(function () {//Change the value for metric units for the label in the weight calculator
+        var metricUnit = $("#metric").val();
+        if (metricUnit === "Feet-pound") {
+            $("#valWeight").html("Pounds");
+            $("#valHeight").html("Feet(s)");
+            $("#valHeight2").html("Inch(es)");
+        } else if (metricUnit === "Meter-KG") {
+            $("#valWeight").html("Kilograms");
+            $("#valHeight").html("Meter(s)");
+            $("#valHeight2").html("Centimeter(s)");
+        }
+    });
+    $("#add").click(function () {//adds and calculate the amount of calories you eat
+        var inFood = $("#foof").val();
+        if (inFood==="Apple(4oz)") {
+
+        }
+    });
+    $("#submit2").click(function () {//Calculate the amount of days you require to lose the amount of weight you want
+        var gender = $("#gender").val();
+        console.log(gender);
+    });
 });
